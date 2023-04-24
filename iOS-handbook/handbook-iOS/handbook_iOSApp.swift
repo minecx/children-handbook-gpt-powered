86a3d6ada2d34af7914eb45a9c848ba3c4ba9bf6
Bob Zhang <cz359@cornell.edu>
2023-04-23 20:34:56 -0400

    iOS starter

---
+++ iOS-handbook/handbook-iOS/handbook_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  handbook_iOSApp.swift
+//  handbook-iOS
+//
+//  Created by Bob on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct handbook_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
