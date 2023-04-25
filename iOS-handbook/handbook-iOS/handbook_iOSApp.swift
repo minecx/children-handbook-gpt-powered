f97b5b670a84bb8656678bca445f775e6c220912
Yun Zhou <yzhou776@gatech.edu>
2023-04-24 21:46:43 -0400

    added chatgpt page

--- iOS-handbook/handbook-iOS/handbook_iOSApp.swift
+++ iOS-handbook/handbook-iOS/handbook_iOSApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct handbook_iOSApp: App {
     var body: some Scene {
         WindowGroup {
-            HomeView()
+//            HomeView()
+            ChatgptView()
         }
     }
 }
