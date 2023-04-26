ceb48c48664c340251c0cfa403f90a64066a0321
CynthiaS321 <131628037+CynthiaS321@users.noreply.github.com>
2023-04-26 17:55:43 -0400

    Update app.py

---
+++ Backend/app.py
@@ -0,0 +1,43 @@
+from db import db, Book, Music, Usser
+from flask import Flask, request
+import json
+import os
+
+app = Flask(__name__)
+db_filename = "cms.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+    
+@app.route("/api/books/")
+def get_books():
+    """
+    get all books
+    """
+    books = [t.serialize() for t in Book.query.all()]
+    return json.dumps({"books": books}), 200
+    
+@app.route("/api/books/<int:book_id>/")
+def get_course(book_id):
+    """
+    get a book by id
+    """
+    book = Book.query.filter_by(id=book_id).first()
+    if book is None:
+        return json.dumps({"error":"Book not found!"}), 404
+    return json.dumps(book.serialize()), 200
+    
+@app.route("/api/users/<int:user_id>/")
+def get_course(user_id):
+    """
+    get a user by id
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return json.dumps({"error":"User not found!"}), 404
+    return json.dumps(user.serialize()), 200
